6c5ca2d63ed3fdb6c5d58568170ae1903646f5ad
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-11-03 15:03:34 -0800

    update map, style, and color

--- oregon-hiking/src/HikingMap.js
+++ oregon-hiking/src/HikingMap.js
@@ -3,23 +3,23 @@ import React from "react";
 const HikingMap = () => {
   const accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
   const baseUrl =
-    "https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/static/";
+    "https://api.mapbox.com/styles/v1/digit9/clxia263s005r01o71y1radny/static/";
   const markers = [
-    "pin-s-star+FF0000(-122.69169,45.50287)",
-    "pin-s-star+FF0000(-120.2528,44.66044)",
-    "pin-s-star+FF0000(-123.97459,45.48588)",
-    "pin-s-star+FF0000(-123.95806,45.80317)",
-    "pin-s-star+FF0000(-122.26228,46.16650)",
-    "pin-s-star+FF0000(-123.11807,42.05520)",
-    "pin-s-star+FF0000(-122.21603,46.27757)",
-    "pin-s-star+FF0000(-123.97003,45.359999)",
-    "pin-s-star+FF0000(-123.97445,45.33132)",
-    "pin-s-star+FF0000(-121.65486,45.40243)",
-    "pin-s-star+FF0000(-117.40668,45.06799)",
-    "pin-s-star+FF0000(-121.0662,43.3729)",
-    "pin-s-star+FF0000(-122.21603,46.27757)",
-    "pin-s-star+FF0000(-122.21505,45.62799)",
-    "pin-s-star+FF0000(-121.83221,45.38696)",
+    "pin-s-star+B4FF00(-122.69169,45.50287)",
+    "pin-s-star+B4FF00(-120.2528,44.66044)",
+    "pin-s-star+B4FF00(-123.97459,45.48588)",
+    "pin-s-star+B4FF00(-123.95806,45.80317)",
+    "pin-s-star+B4FF00(-122.26228,46.16650)",
+    "pin-s-star+B4FF00(-123.11807,42.05520)",
+    "pin-s-star+B4FF00(-122.21603,46.27757)",
+    "pin-s-star+B4FF00(-123.97003,45.359999)",
+    "pin-s-star+B4FF00(-123.97445,45.33132)",
+    "pin-s-star+B4FF00(-121.65486,45.40243)",
+    "pin-s-star+B4FF00(-117.40668,45.06799)",
+    "pin-s-star+B4FF00(-121.0662,43.3729)",
+    "pin-s-star+B4FF00(-122.21603,46.27757)",
+    "pin-s-star+B4FF00(-122.21505,45.62799)",
+    "pin-s-star+B4FF00(-121.83221,45.38696)",
   ];
   const center = "-120.5,44.0,6";
   const dimensions = "700x1000";
